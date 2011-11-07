07504b807494bee0551adccf4c11ee20a8782a51
mattuk <makampf@gmail.com>
2011-11-07 18:20:09 +0100

    Added a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ SampleApp2::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  get "pages/home"
+  get "pages/contact"
 end
